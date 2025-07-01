7c230d11a318309d5d556fd2f0e8ec324a93c7c9
ADESH MAHTO <141711491+AdeshStack@users.noreply.github.com>
2025-07-01 07:54:12 +0530

    Create WavyPattern.java

---
+++ WavyPattern.java
@@ -0,0 +1,14 @@
+public class WavyPattern {
+    public static void main(String[] args) throws InterruptedException {
+        for (int row = 0; row < 20; row++) {
+            for (int col = 0; col < 80; col++) {
+                if (Math.sin((col + row) * 0.2) > 0.8)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+            Thread.sleep(100); // delay for wave motion
+        }
+    }
+}
